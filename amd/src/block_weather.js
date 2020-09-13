35e59ebc59bdca4640789b1b79529c07d2fee822
“Safat <safat@ecreators.com.au>
2020-09-13 12:39:53 +1000

    cencius conversion

--- amd/src/block_weather.js
+++ amd/src/block_weather.js
@@ -13,6 +13,27 @@ define(['jquery', 'core/ajax', 'core/str', 'core/config', 'core/notification', '
                         tag.html(html);
                         weather.weatherInfo(s, enabledOption, openWeatherKey);
                     });
+                    $(document).on('click', '.block-weather-temperature-value', function() {
+                        var tempValue = $('.block-weather-temperature-value').attr('data-value');
+                        if (tempValue !== '-') {
+                            var tempAction = $('.block-weather-temperature-value').attr('data-action');
+                            if (tempAction === 'celsius') {
+                                var farhValue = Math.floor((parseInt(tempValue) * 9 / 5) + 32);
+                                var temp = $('.block-weather-temperature-value');
+                                var tempData = '<p>' + farhValue + '°<span>F</span></p>';
+                                $('.block-weather-temperature-value').attr('data-value', farhValue);
+                                $('.block-weather-temperature-value').attr('data-action', 'fahrenheit');
+                                temp.html(tempData);
+                            } else {
+                                var celcValue = Math.floor((parseInt(tempValue) - 32) * 5 / 9);
+                                var temp = $('.block-weather-temperature-value');
+                                var tempData = '<p>' + celcValue + '°<span>C</span></p>';
+                                $('.block-weather-temperature-value').attr('data-value', celcValue);
+                                $('.block-weather-temperature-value').attr('data-action', 'celsius');
+                                temp.html(tempData);
+                            }
+                        }
+                    });
                 });
             },
             weatherInfo: function(s, enabledOption, openWeatherKey) {
@@ -45,7 +66,8 @@ define(['jquery', 'core/ajax', 'core/str', 'core/config', 'core/notification', '
                 var context = {
                     weathererror: `<p> ${error} </p>`,
                     weathericon: M.util.image_url('unknown', 'block_weather'),
-                    weathervalue: '-'
+                    weathervalue: '-',
+                    weatherattr: '-'
                 };
                 templates.render('block_weather/weather_info', context).then(function(html, js) {
                     var tag = $('#weather-container');
@@ -63,6 +85,7 @@ define(['jquery', 'core/ajax', 'core/str', 'core/config', 'core/notification', '
                             var context = {
                                 weathervalue: Math.floor(data.main.temp - 273),
                                 weatherdesc: data.weather[0].description,
+                                weatherattr: 'celsius',
                                 weathericon: M.util.image_url(data.weather[0].icon, 'block_weather'),
                                 weatherlocation: data.name + ',' + data.sys.country
                             };
